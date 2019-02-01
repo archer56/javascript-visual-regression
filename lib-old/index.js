e9c748b62e3c2d8839917138bc1f619967b45816
archer56 <ben.archer@hotmail.co.uk>
2019-02-01 13:21:38 +0000

    removed old jvr

--- lib-old/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-require('./babel.js');
-const Config = require('./services/config');
-const package = require('../package.json');
-const config = Config.fetchFromProject();
-
-console.log(config)
